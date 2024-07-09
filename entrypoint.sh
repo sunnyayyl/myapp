c82f09bfe50da38df8a6c13d8e472819d92fe018
sunny.ayyl <sunny.ayyl@gmail.com>
2024-07-09 17:09:54 +0000

    Remove cp and use -o flag on flutter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 flutter pub get
-flutter build web -o docs --base-href /myapp/
-cp ./build/web /github/workspace/web-built -r
+flutter build web -o /github/workspace/web-built --base-href /myapp/
