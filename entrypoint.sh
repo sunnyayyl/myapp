e35025d9e9963e08aa220283568fb38d5da97f89
sunny.ayyl <sunny.ayyl@gmail.com>
2024-07-09 17:41:02 +0000

    fix apk upload for real

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 flutter pub get
 flutter build web -o /github/workspace/web-artifact --base-href /myapp/
-flutter build apk -o /github/workspace/apk-artifact
+flutter build apk
 cp ./build/app/outputs/flutter-apk/app-release.apk /github/workspace/app-release.apk
