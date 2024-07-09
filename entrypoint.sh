42d98d7260922501bd218922857427d439538270
sunny.ayyl <sunny.ayyl@gmail.com>
2024-07-09 17:34:12 +0000

    Fix apk artifact

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,4 @@
 flutter pub get
 flutter build web -o /github/workspace/web-artifact --base-href /myapp/
 flutter build apk -o /github/workspace/apk-artifact
+cp build/app/outputs/flutter-apk/app-release.apk /github/workspace/app-release.apk
