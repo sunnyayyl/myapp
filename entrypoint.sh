af11792b93c446a37fed5344f4b33b7b59e3acae
sunny.ayyl <sunny.ayyl@gmail.com>
2024-07-13 21:20:49 +0000

    Use wasm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 flutter pub get
-flutter build web -o /github/workspace/web-artifact --base-href /myapp/
+flutter build web -o /github/workspace/web-artifact --base-href /myapp/  --wasm
 flutter build apk
 cp ./build/app/outputs/flutter-apk/app-release.apk /github/workspace/app-release.apk
